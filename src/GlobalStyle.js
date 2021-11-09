14bd8d302ec4c0e9c0f0855952f49d856b5e4c3c
youhane <jason.sebastian154@gmail.com>
2021-11-09 09:46:01 +0700

    go

---
+++ src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --defaultGrey: hsla(0, 0%, 88%, 1);
+        --hoverGrey: hsla(0, 0%, 68%, 1);
+        --outlineBlue: hsla(231, 99%, 62%, 1);
+        --hoverBlue: hsla(224, 100%, 58%, 0.1);
+        --defaultBlue: hsla(231, 99%, 62%, 1);
+        --white: #FFFFFF;
+    }
+`
