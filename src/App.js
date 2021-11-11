97d2ffda93e9821107ccc4210d08afe3b1b564fd
youhane <jason.sebastian154@gmail.com>
2021-11-11 11:06:13 +0700

    hm

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const App = () => (
   <div className="App">
     <h1>Buttons</h1>
     <Button />
+    <Button variant="outline" />
+    <Button variant="text" />
+    <Button disableShadow />
+    <Button disabled />
+    <Button variant="text" disabled />
+    <Button startIcon="shopping-cart" />
     <GlobalStyle />
   </div>
 );
